65da3512c77ed1b714684f19c9c3d868637bc950
Anderson Losada Coder <andersonlosada.11@gmail.com>
2025-05-19 05:47:41 -0500

    First commit

---
+++ src/auth/pages/LoginPage.jsx
@@ -0,0 +1,22 @@
+import { replace, useNavigate } from 'react-router-dom';
+
+export const LoginPage = () => {
+  const navigate = useNavigate();
+
+  const onLogin = () => {
+    navigate('/', {
+      replace: true,
+    });
+  };
+
+  return (
+    <div>
+      <h1 className='container mt-5'>Login</h1>
+      <hr />
+
+      <button className='btn btn-primary' onClick={onLogin}>
+        Login
+      </button>
+    </div>
+  );
+};
