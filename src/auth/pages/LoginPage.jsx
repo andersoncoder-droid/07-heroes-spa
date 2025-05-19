c013f7e631a81d4f242ec6f7502d7a9ad3de208c
Anderson Losada Coder <andersonlosada.11@gmail.com>
2025-05-19 16:08:09 -0500

    Authentication context with login/logout ✅

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,10 +1,17 @@
-import { replace, useNavigate } from 'react-router-dom';
+import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { AuthContext } from '../context/AuthContext';
 
 export const LoginPage = () => {
+  const { login } = useContext(AuthContext);
   const navigate = useNavigate();
 
   const onLogin = () => {
-    navigate('/', {
+    const lastPath = localStorage.getItem('lastPath') || '/';
+
+    login('Anderson Losada');
+
+    navigate(lastPath, {
       replace: true,
     });
   };
