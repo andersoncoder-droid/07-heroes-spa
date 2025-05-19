bab2769c1d4fb9e1f7ff9d2770c610e650906649
Anderson Losada Coder <andersonlosada.11@gmail.com>
2025-05-19 06:03:50 -0500

    SearchPage ⭐

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -11,13 +11,16 @@ export const SearchPage = () => {
   const { q = '' } = queryString.parse(location.search);
   const heroes = getHeroesByName(q);
 
+  const showSearch = q.length === 0;
+  const showError = q.length > 0 && heroes.length === 0;
+
   const { searchText, onInputChange } = useForm({
     searchText: '',
   });
 
   const onSearchSubmit = (event) => {
     event.preventDefault();
-    if (searchText.trim().length <= 1) return;
+    // if (searchText.trim().length <= 1) return;
 
     navigate(`?q=${searchText}`);
   };
@@ -47,8 +50,28 @@ export const SearchPage = () => {
         <div className='col-7'>
           <h4>Results</h4>
           <hr />
-          <div className='alert alert-primary'>Search a hero</div>
-          <div className='alert alert-danger'>
+
+          {/* {q === '' ? (
+            <div className='alert alert-primary'>Search a hero</div>
+          ) : (
+            heroes.length === 0 && (
+              <div className='alert alert-danger'>
+                There's no results for <b>{q}</b>
+              </div>
+            )
+          )} */}
+
+          <div
+            className='alert alert-primary animate__animated animate__fadeIn'
+            style={{ display: showSearch ? '' : 'none' }}
+          >
+            Search a hero
+          </div>
+
+          <div
+            className='alert alert-danger animate__animated animate__fadeIn'
+            style={{ display: showError ? '' : 'none' }}
+          >
             There's no results for <b>{q}</b>
           </div>
 
